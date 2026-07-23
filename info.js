14d91aac939c01f1dbac75012a54bc40d8ad0434
Tikrack <tikrackcode@gmail.com>
2026-07-23 11:28:45 +0330

    fix info.js bug

--- info.js
+++ info.js
@@ -1,1 +1,18 @@
-ز
+const fs = require('fs').promises;
+const path = require('path');
+
+async function getInfo() {
+  try {
+    const filePath = path.join(__dirname, 'info.json');
+    
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    
+    const data = JSON.parse(fileContent);
+    
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = getInfo;
